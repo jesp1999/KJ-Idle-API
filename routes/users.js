3f12960267cf0431cd60eec0e4ed078b65566aff
jesp1999 <jesp1999@mit.edu>
2022-04-20 01:56:46 -0400

    Put userdb management under users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
+const bcrypt = require("bcrypt")
+const { MongoClient, ServerApiVersion, MongoCursorInUseError } = require('mongodb');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+require("dotenv").config();
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
+
+const db = { //TODO make this a mongo connection pool
+   connectionLimit: 100,
+   host: DB_HOST,
+   user: DB_USER,
+   password: DB_PASSWORD,
+   database: DB_DATABASE,
+   port: DB_PORT
+};
+//remember to include .env in .gitignore file
+
+const uri = 'mongodb+srv://' + encodeURIComponent(db.user) + ':' + encodeURIComponent(db.password) + '@' + db.host;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//CREATE USER
+router.post('/createUser', async (req,res,next) => {
+    const username = req.body.username;
+    const hashedPassword = await bcrypt.hash(req.body.password,10);
+
+    client.connect(async err => {
+        if (err) throw (err);
+        const collection = client.db(db.database).collection('userDB'); //todo make tablename an env variable?
+
+        // if entry matching username exists in userDB, don't add new one
+        collection.find({Username: username}).hasNext().then(userAlreadyExists => {
+            if (userAlreadyExists > 0) {
+                //TODO throw an error, user already exists!
+                console.log('------> User already exists');
+                res.sendStatus(409) ;
+            } else {
+                collection.insertOne({
+                    Username: username,
+                    HashedPassword: hashedPassword
+                });
+                console.log ('--------> Created new User'); //TODO log user IDs
+                res.sendStatus(201);
+            }
+        });
+    }); //end of client.connect()
+}) //end of app.post()
 
 module.exports = router;
